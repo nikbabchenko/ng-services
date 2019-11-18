6e0320276f0ac0b2ae9974ab28063ead8b3470a0
mbabchenko <mbabchenko@odl1905011.lohika.com>
2019-11-18 16:23:20 +0200

    Adds users

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { UsersRoutingModule } from './users-routing.module';
 import { UsersComponent } from './pages/users/users.component';
+import { UserCardComponent } from './components/user-card/user-card.component';
+import { UserListComponent } from './components/user-list/user-list.component';
 
 @NgModule({
-  declarations: [UsersComponent],
+  declarations: [UsersComponent, UserCardComponent, UserListComponent],
   imports: [CommonModule, UsersRoutingModule]
 })
 export class UsersModule {}
